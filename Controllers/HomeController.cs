1ad8a2c3eb9953a55c20210afe9a067f0d7ed643
FidanMemmedova <5TAWDL9@code.edu.az>
2022-06-23 02:31:34 +0400

    bezdim

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DAL;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController:Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Fullname { get; set; }
+        public string Position { get; set; }
+    }
+}
